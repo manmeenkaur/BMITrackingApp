a973562d34027e5c46520817fc19b9c82109e318
manmeenkaur <manmeen.kaur.17@gmail.com>
2022-12-16 23:36:25 -0500

    Added Edit View Controller

---
+++ BMITrackingApp/EditBMIViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EditBMIViewController.swift
+//  BMITrackingApp
+//
+//  Created by Manmeen Kaur on 2022-12-16.
+//
+
+import UIKit
+
+class EditBMIViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
